ea397585ebadb268b3c4f66755d5b93f45113c60
Pablo Ferrer-Dalmau <pablo@Pablo>
2021-06-10 15:08:03 +0200

    10/06/2021

--- abfp/client.sh
+++ abfp/client.sh
@@ -36,27 +36,43 @@ echo "(6) Handshake"
 sleep 1
 echo "THIS_IS_MY_CLASSROOM" | nc -q 1 $IP_SERVER $PORT
 
-echo "(7) Listening"
+echo "(7a) Listening HANDSHAKE RESPONSE..."
 
 RESPONSE=`nc -l -p $PORT`
 if [ "$RESPONSE" != $YES ]; then
-	echo "Error: Connection refused"
+	echo "Handshake fallido"
 	exit 2
 fi
 
-echo "(10) File name"
-
+echo "(7b) SENDING NUM_FILES"
 sleep 1
-MD5=`echo $FILE_NAME | md5sum | cut -d " " -f 1`
-echo "FILE_NAME $FILE_NAME $MD5" | nc -q 1 $IP_SERVER $PORT
+NUM_FILES =`ls $INPUT_PATH | wc -w`
+echo "NUM_FILES NUM_FILES" | nc -q 1 $IP_SERVER $PORT
 
-echo "(11) Listening"
-
+echo "(7c) Listen"
 RESPONSE=`nc -l -p $PORT`
-if [ "$RESPONSE" != $OK_FILE ]; then
-	echo "Error: something happened with the file name"
-	exit 3
+if [ "$RESPONSE" != "OK_NUM_FILES" ]; then
+	echo "ERROR: NUM_FILES incorrecto"
+	exit 2
 fi
+
+for FILE_NAME in `ls $INPUT_PATH`; do
+	FILE_MD5= `echo $FILE_NAME | md5sum | cut -d " " -f 1`
+	
+	echo "(10 Sending File"
+	sleep 1
+	echo "File_NAME $FILE_NAME $FILE_MD5" | nc -q 1 $IP_SERVER $PORT
+
+	echo "(11) Listening FILE_NAME RESPONSE"
+	RESPONSE = `nc -l -p $PORT`
+	if [ "$RESPONSE" != "OK_FILE_NAME" ]; then
+		echo "ERROr: CORRUPT FILE"
+		exit 3
+	fi
+	sleep 1
+	cat "$INPUT_PATH$FILE_NAME" | nc -q 1 $IP_SERVER $PORT
+done
+
 
 echo "(14) Data"
 
--- abfp/server.sh
+++ abfp/server.sh
@@ -1,80 +1,133 @@
 #!/bin/bash
-
 PORT=2021
-PORT_NAME="ABFP"
-HANDSHAKE="THIS_IS_MY_CLASSROOM"
+OUTPUT_PATH="server_output/"
 
-echo "(0) Alejandro's Big Fucking Protocol"
+echo "(0) Server ABFP"
 
-echo "(1) Listening $PORT"
+echo "(1) Listening ($PORT) Headers"
 
-HEADER=`nc -l -p $PORT`
-PREFIX=`echo $HEADER | cut -d " " -f 1`
+HEADER= `nc -l -p $PORT`
+echo "ABFP TESTIng $HEADER"
+
+PREFIX= `echo $HEADER | cut -d " " -f 1`
 IP_CLIENT=`echo $HEADER | cut -d " " -f 2`
 
-echo "TEST HEADER"
-
 echo "(4) Response"
-
-if [ "$PREIX" != $PORT_NAME ]; then
-	echo "Error: Wrong header"
+if [ "$PREFIX" != "ABFP" ]; then
+	echo "ERROR: IN HEADER"
 	sleep 1
 	echo "KO_CONN" | nc -q 1 $IP_CLIENT $PORT
+
 	exit 1
 fi
-
 sleep 1
 echo "OK_CONN" | nc -q 1 $IP_CLIENT $PORT
 
-echo "(5) Listening"
-
-CLIENT_HANDSHAKE=`nc -l -p $PORT`
+echo "(5) Listening Test"
+HANDSHAKE=`nc -l -p $PORT`
+echo "Handshake testing $HANDSHAKE"
 
 echo "(8) Response"
-
-if [ "$CLIENT_HANDSHAKE" != $HANDSHAKE ]; then
-	echo "Errpr: Wrong handshake"
+if [ "$HANDSHAKE" != "THIS_IS_MY_CLASSROOM" ]; then
+	echo "ERROR Handshake"
 	sleep 1
 	echo "KO_HANDSHAKE" | nc -q 1 $IP_CLIENT $PORT
+fi
+sleep 1
+echo "YES_IT_IS" | nc -q 1 $IP_CLIENT $PORT
+
+echo "(8b) Listen NUM_FILES"
+
+NUM_FILES=`nc -l -p $PORT`
+PREFIX=`echo $NUM_FILES | cut -d " " -f 1`
+NUM=`echo $NUM_FILES | cut -d " " -f 2`
+
+if [ "$PREFIX" != "NUM_FILES" ]; then
+	echo "ERROR: NUM_FILES incorrecto"
+	sleep 1
+	echo "KO_NUM_FILES" | nc -q 1 $IP_CLIENT $PORT
+	
 	exit 2
 fi
 
 sleep 1
+echo "OK_NUM_FILES" | nc -q 1 $IP_CLIENT $PORT
+echo "NUM_FILES: $NUM"
 
-echo "YES_IT_IS" | nc -q 1 $IP_CLIENT $PORT
+for NUMBER in `seq $NUM`; do
+	echo "(9) Listening"
+	FILE_NAME= `nc -l -p $PORT`
 
-echo "(9) Listening"
+	PREFIX=`echo $FILE_NAME | cut -d " " -f 1`
+	NAME=`echo $FILE_NAME | cut -d " " -f 2`
+	NAME_MD5=`echo $FILE_NAME | cut -d " " -f 3`
 
-FILE_NAEME=`nc -l -p $PORT`
-PREFIX=`echo $FILE_NAME | cut -d " " -f 1`
-NAME=`echo $FILE_NAME | cut -d " " -f 2`
-MD5_NAME=`echo $FILE_NAME | cut -d " " -f 3`
+	if [ "$PREFIX" != "FILE_NAME" ]; then
+		echo "ERROR  in FILE_NAME"
+		sleep 1
+		echo "KO_FILE_NAME" | nc -q 1 $IP_CLIENT $PORT
+		exit 3
+	fi
+	TEMP_MD5=`echo $NAME | md5sum |cut -d " " -f 1`
 
-echo "(12) Response"
+	if [ "$NAME_MD5" != "$TEMP_MD5" ]; then
+		echo "ERROR in FILE_NAME"
+		sleep 1
+		echo "KO_FILE_NAME" | nc -q 1 $IP_CLIENT $PORT
+		exit 4
+	fi
 
-if [ "$PREFIX" != "FILE_NAME" ]; then
-	echo "Error: wrong prefix"
+	echo "(12) FILE_NAME($NAME) Response"
 	sleep 1
-	echo "KO_FILE_NAME" | nc -q 1 $IP_CLIENT $PORT
-	exit 3
-fi
+	echo "OK_FILE_NAME" | nc -q 1 $IP_CLIENT $PORT
+	echo $OUTPUT_PATH$NAME
 
-MD5_CHECK=`echo $NAME | md5sum | cut -d " " -f 1`
+	nc -l -p $PORT > $OUTPUT_PATH$NAME
+done
+echo "(13) Listening"
+DATA= `nc -l -p $PORT`
+MD5_CHECK=`md5sum $FILE | cut -d " " -f 1`
+DATA_NAME=`echo $DATA | cut -d " " -f 2`
+DATA_MD5=`echo $DATA | cut -d " " -f 3`
 
-if [ "$MD5_CHECK" != "$MD5_NAME" ]; then
-	echo "Error: wrong file name"
+if [ "$MD5_CHECK" != "$DATA_MD5" ]; then
 	sleep 1
-	echo "KO_FILE_NAME" | nc -q 1 $IP_CLIENT $PORT
+	echo "Server MD5 : $MD5_CHECK"
+	echo "Client MD5 : $DATA_MD5"
+	sleep 1
+	echo "DATA STATUS: Corrupto"
+	echo "KO_DATA" | nc -q 1 $IP_CLIENT $PORT
+	echo "Connection status: Failed " | mail -s "ABFP_Admin" alejandor_test@mailinator.com
 	exit 4
 fi
-
 sleep 1
-echo "OK_FILE_NAME" | nc -q 1 $IP_CLIENT $PORT
+echo "(16) FILE_STATUS Response"
+echo "OK_DATA" | nc -q 1 $IP_CLIENT $PORT
+echo "File Status: OK ! $MD5_CHECK / $DATA_MD5" | mail -s 'ABFP-Admin' alejandro_test@mailinator.com
+exit 0
 
-echo "(12b) mail"
-echo "$NAME" | mail -s "Nombre del archivo recibido" alejandro_test@mailinator.com
 
-echo "(13) Listening"
-nc -l -p $PORT > archivo_entrado.vaca
-cat archibo_entrada.vaca
-exit0 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
